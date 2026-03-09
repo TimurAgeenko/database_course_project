48ddabc93a0ca308d49fc42d919792919217eff3
TimurAgeenko <ageenko.timur.10@gmail.com>
2026-03-09 15:53:03 +0300

    Added get_employers_id method and test for it

--- src/api.py
+++ src/api.py
@@ -1,7 +1,8 @@
-import requests, os
-
-from dotenv import load_dotenv
+import os
 from typing import Any
+
+import requests
+from dotenv import load_dotenv
 
 
 class HeadHunterAPIHandler:
@@ -10,17 +11,34 @@ class HeadHunterAPIHandler:
     def __init__(self):
         load_dotenv()
         self.__url = os.getenv("HEADHUNTER_API_URL")
-        self.__headers = {
-            "User-Agent": os.getenv("HEADHUNTER_HEADER"),
-        }
+        self.__headers = {"User-Agent": os.getenv("HEADHUNTER_HEADER")}
         self.__employers_id = []
 
     def __repr__(self):
         return f"URL: {self.__url}, headers: {self.__headers["User-Agent"]}"
 
-    def get_employers_id(self, employers: list[str]) -> int:
+    def get_employers_id(self, employers: list[str]) -> dict[str, Any]:
         """Метод для получения идентификаторов работодателей по их названиям."""
-        pass
+        result = {}
+
+        for employer in employers:
+            employers_dict = {}
+            params = {
+                "text": employer,
+                "only_with_vacancies": True,
+                "sort_by": "by_vacancies_open"
+            }
+
+            response = requests.get(self.__url, headers=self.__headers, params=params)
+            response_json = response.json()
+
+            for item in response_json["items"]:
+                employers_dict[item["name"]] = item["id"]
+
+            result["requested_name"] = employer
+            result["founded_employers"] = employers_dict
+
+        return result
 
     def get_vacancies(self) -> list[dict[str, Any]]:
         """Метод для получения списка с вакансиями по работодателям."""
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,7 +1,39 @@
+import os
+from unittest.mock import patch
+
+from dotenv import load_dotenv
+
 from src.api import HeadHunterAPIHandler
 
 
 def test_headhunter_api_initialization():
     api_handler = HeadHunterAPIHandler()
-    api_handler_repr = "URL: https://api.hh.ru/, headers: HeadHunter's Hunter/1.0 (ageenko.timur.10@gmail.com)"
+    api_handler_repr = (
+        "URL: https://api.hh.ru/employers, headers: HeadHunter's Hunter/1.0 (ageenko.timur.10@gmail.com)"
+    )
     assert str(api_handler) == api_handler_repr
+
+
+@patch("requests.get")
+def test_headhunter_api_get_employers_id(mock_get):
+    api_handler = HeadHunterAPIHandler()
+
+    mock_get.return_value.json.return_value = {
+        "items": [
+            {"id": 1, "name": "emp1"},
+            {"id": 2, "name": "emp2"},
+            {"id": 3, "name": "emp3"}
+        ]
+    }
+
+    employers_info = api_handler.get_employers_id(["Example"])
+
+    assert employers_info == {"requested_name": "Example", "founded_employers": {"emp1": 1, "emp2": 2, "emp3": 3}}
+
+    load_dotenv()
+    url = os.getenv("HEADHUNTER_API_URL")
+    headers = {"User-Agent": os.getenv("HEADHUNTER_HEADER")}
+
+    params = {"text": "Example", "only_with_vacancies": True, "sort_by": "by_vacancies_open"}
+
+    mock_get.assert_called_once_with(url, headers=headers, params=params)
